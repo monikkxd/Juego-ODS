61571dcb6dd84e50e9708645b44531d5e6b1151c
Mónica García Sebastián <monicagarciaask@gmail.com>
2024-02-09 12:40:08 +0100

    Pequeños Ajustes

--- JuegoODS/Assets/_MinijuegoMoni/Scripts_MoniQ/CasillasWinScript.cs
+++ JuegoODS/Assets/_MinijuegoMoni/Scripts_MoniQ/CasillasWinScript.cs
@@ -6,7 +6,7 @@ public class CasillasWinScript : MonoBehaviour
 {
     private void OnTriggerEnter(Collider other)
     {
-        if(other.CompareTag("WinTag"))
+        if(other.CompareTag("Victory_Collider"))
         {
             Debug.Log("Victoria");
         }
