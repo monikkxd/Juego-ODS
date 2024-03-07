56a712596aa05e50e7e267759fa013a180c2945c
HugoRufete <hugorufete@gmail.com>
2024-03-07 15:22:50 +0100

    Lógica Depositar Plato Pulida

--- JuegoODS/Assets/CogerPlatos.cs
+++ JuegoODS/Assets/CogerPlatos.cs
@@ -7,28 +7,76 @@ public class CogerPlatos : MonoBehaviour
     public GameObject whatCanIPickUp;
     public GameObject holder;
 
-
     private void Update()
     {
-        if(Input.GetKeyDown(KeyCode.E))
+        if (Input.GetKeyDown(KeyCode.E))
         {
-            PickUpObject();
+            if (whatCanIPickUp != null)
+            {
+                if (whatCanIPickUp.transform.parent == holder.transform)
+                {
+                    // Already holding an object, check if we are over a table to deposit
+                    GameObject detectedTable = GetDetectedTable();
+                    if (detectedTable != null)
+                    {
+                        // Deposit the object on the detected table
+                        DepositObject(detectedTable);
+                    }
+                }
+                else
+                {
+                    // Not holding an object, pick up if over a plate
+                    PickUpObject();
+                }
+            }
         }
     }
+
     private void OnTriggerEnter(Collider other)
     {
-        if(other.CompareTag("Plato"))
+        if (other.CompareTag("Plato"))
         {
             whatCanIPickUp = other.gameObject;
-            Debug.Log("Se puede coger" + other.gameObject.name);
+            Debug.Log("Se puede coger " + other.gameObject.name);
         }
+    }
+
+    bool IsCollidingWithTable()
+    {
+        // Check if the object with this script is colliding with an object tagged as "Mesa"
+        Collider[] colliders = Physics.OverlapBox(transform.position, transform.localScale / 2, transform.rotation, LayerMask.GetMask("Mesa"));
+
+        return colliders.Length > 0;
     }
 
     void PickUpObject()
     {
         whatCanIPickUp.transform.SetParent(holder.transform);
-        //whatCanIPickUp.transform.localScale = new Vector3(1f, 1f, 1f);
         whatCanIPickUp.transform.localPosition = new Vector3(0, 0, 0);
     }
 
+    void DepositObject(GameObject table)
+    {
+        // Obtener la posición local deseada en la mesa
+        Vector3 depositPosition = new Vector3(0.1f, 0.3f, 0.0f); // Ajusta esto según tus necesidades
+
+        // Establecer la posición y el padre del objeto que se está sosteniendo
+        whatCanIPickUp.transform.SetParent(table.transform);
+        whatCanIPickUp.transform.localPosition = depositPosition;
+
+        // Añade cualquier lógica adicional que puedas necesitar al depositar el objeto
+    }
+
+
+
+    GameObject GetDetectedTable()
+    {
+        // Get the object with the tag "Mesa" that the script is currently colliding with
+        Collider[] colliders = Physics.OverlapBox(transform.position, transform.localScale / 2, transform.rotation, LayerMask.GetMask("Mesa"));
+        if (colliders.Length > 0)
+        {
+            return colliders[0].gameObject;
+        }
+        return null;
+    }
 }
