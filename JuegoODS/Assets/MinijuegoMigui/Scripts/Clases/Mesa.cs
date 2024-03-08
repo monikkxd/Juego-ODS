55637e9834dada9e5fcb61f494bac41eae1376cd
Mónica García <monicagarciaask@gmail.com>
2024-03-08 12:28:46 +0100

    Detección Plato (Zelling Racista + Komanche Majero)

--- JuegoODS/Assets/MinijuegoMigui/Scripts/Clases/Mesa.cs
+++ JuegoODS/Assets/MinijuegoMigui/Scripts/Clases/Mesa.cs
@@ -11,14 +11,36 @@ public class Mesa : MonoBehaviour
             Debug.Log("Cliente ha llegado a la mesa");
             clienteEnMesa = true;
         }
+
+        if (other.CompareTag("Plato"))
+        {
+            Debug.Log("Plato Detecto");
+        }
+        else
+        {
+            Debug.Log("No se ha detectado plato");
+        }
+
     }
 
+    /*private void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("Plato"))
+        {
+            Debug.Log("Plato Detecto");
+        }
+        else
+        {
+            Debug.Log("No se ha detectado plato");
+        }
+    }
+*/
     void OnTriggerExit(Collider other)
     {
         if (other.CompareTag("Cliente"))
         {
             Debug.Log("Cliente ha dejado la mesa");
-            clienteEnMesa = false;
+            clienteEnMesa = true;
         }
     }
 }
--- JuegoODS/Assets/MinijuegoMigui/Scripts/Clases/Plato.cs
+++ JuegoODS/Assets/MinijuegoMigui/Scripts/Clases/Plato.cs
@@ -6,7 +6,6 @@ public class Plato : MonoBehaviour
     //public string tipo;
     public void Interactuar()
     {
-        //SceneManager.LoadScene("MinigameSelectionClara");
         Debug.Log("Interactuando con: " + gameObject.name);
     }
 }
