1a4b38c6fb40249d9ed2ae0962bf496385ad905e
monikkxd <monicagarciaask@gmail.com>
2024-03-25 02:44:05 +0100

    character controller hecho

---
+++ JuegoODS/Assets/_MinijuegoNatalia/CharacterController2D.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController2D : MonoBehaviour
+{
+    public float speed = 5f;
+    public float verticalSpeed = 5f; // Velocidad vertical
+
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        // Movimiento horizontal
+        float moveInput = Input.GetAxisRaw("Horizontal");
+        rb.velocity = new Vector2(moveInput * speed, rb.velocity.y);
+
+        // Movimiento vertical
+        float moveInputVertical = Input.GetAxisRaw("Vertical");
+        rb.velocity = new Vector2(rb.velocity.x, moveInputVertical * verticalSpeed);
+    }
+}
+
