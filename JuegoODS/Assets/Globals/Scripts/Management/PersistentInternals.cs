4a64802d4255709d32775dd45fc5a04b914dc213
monikkxd <monicagarciaask@gmail.com>
2024-04-24 18:55:01 +0200

    CHARACTER CONTROLLER DE LOS COJONES EN ISOMETRICA PERFECTO 0 FALLOS

--- JuegoODS/Assets/Globals/Scripts/Management/PersistentInternals.cs
+++
@@ -1,8 +0,0 @@
-/// <summary>
-/// Esto solo sirve como declaración de objeto persistente
-/// Lo importante es que los objetos anidados en el Prefab de PersistentElements serán persistentes en la totalidad del juego
-/// </summary>
-public class PersistentInternals : PersistentSingleton<PersistentInternals>
-{
-    // Aquí no va nada xd
-}
