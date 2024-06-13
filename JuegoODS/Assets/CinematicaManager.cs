822fd1f84d18edf5a02263fc4b88e9fccb19bf98
HugoRufete <hugorufete@gmail.com>
2024-06-13 18:39:32 +0200

    Cinemática - Acelerar x4 & Saltar Cinemática

---
+++ JuegoODS/Assets/CinematicaManager.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CinematicaManager : MonoBehaviour
+{
+    public GameObject fadeIn;
+
+    public Animator animator;
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.Space))
+        {
+            //animator.SetBool("FadeIn", true);
+            fadeIn.SetActive(true);
+            Invoke("CambioEscena", 2.3f);
+        }
+        
+        if(Input.GetKey(KeyCode.E))
+        {
+            Time.timeScale = 4f;
+        }
+        else
+        {
+            Time.timeScale = 1.0f;
+        }
+    }
+
+    void CambioEscena()
+    {
+        SceneManager.LoadScene("PrimeraIsla");
+    }
+}
