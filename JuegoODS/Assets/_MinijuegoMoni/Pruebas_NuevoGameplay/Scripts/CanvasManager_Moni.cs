9893e0fc76527e590eceb37cbc49d6a6f5428dc0
HugoRufete <hugorufete@gmail.com>
2024-06-21 14:04:27 +0200

    Lógica Animaciones Sistema Construcción

--- JuegoODS/Assets/_MinijuegoMoni/Pruebas_NuevoGameplay/Scripts/CanvasManager_Moni.cs
+++ JuegoODS/Assets/_MinijuegoMoni/Pruebas_NuevoGameplay/Scripts/CanvasManager_Moni.cs
@@ -6,9 +6,9 @@ public class CanvasManager_Moni : MonoBehaviour
 {
     public Animator animator;
 
-    private bool interfazCambiada = false;
-    private bool interfazCambiada2 = false;
-    private bool interfazCambiada3 = false;
+    [SerializeField] private bool interfazCambiada = false;
+    [SerializeField] private bool interfazCambiada2 = false;
+    [SerializeField] private bool interfazCambiada3 = false;
 
     private void Start()
     {
@@ -16,14 +16,39 @@ public class CanvasManager_Moni : MonoBehaviour
     }
     public void CambioInterfaz()
     {
-        animator.enabled = true;
+        if(interfazCambiada == false)
+        {
+            animator.enabled = true;
 
+            interfazCambiada = true;
+
+            animator.Play("CambioInterfaz_Animation");
+        }
+        else if(interfazCambiada == true & interfazCambiada2 == false)
+        {
+            interfazCambiada2 = true;
+            interfazCambiada3 = true;
+            animator.Play("CambioInterfaz3_Animation");
+        }
 
     }
 
     public void CambioInterfaz2()
     {
-        
+        if(interfazCambiada == true && interfazCambiada2 == false)
+        {
+            interfazCambiada = false;
+            interfazCambiada2 = false;
+
+            animator.Play("CambioInterfaz2_Animation");
+        }
+        else if(interfazCambiada3 == true)
+        {
+            interfazCambiada = true;
+            interfazCambiada2 = false;
+            interfazCambiada3 = false;
+            animator.Play("CambioInterfaz4_Animation");
+        }
     }
 
 
