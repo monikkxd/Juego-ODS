0768e9a7da2d13b48a6ffdeb3e9a7e7f6a9f2a97
monikkxd <monicagarciaask@gmail.com>
2024-06-22 18:35:16 +0200

    logica cambio de materal andrea

---
+++ JuegoODS/Assets/_MinijuegoAndrea/CambioMat.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CambioMat : MonoBehaviour
+{
+    public GameObject plataforma;
+    public Material materialVerde;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter(Collider collider)
+    {
+        Debug.Log(collider.name);
+        Renderer rend = plataforma.GetComponent<Renderer>();
+        if (collider.tag == "MANGUERA")
+        {
+            rend.material = materialVerde;
+            Debug.Log("TOCADO " + collider.gameObject.name);
+        }
+    }
+}
