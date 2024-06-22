eb594402da2306dd807f4e6a6138d3a058b24255
monikkxd <monicagarciaask@gmail.com>
2024-06-22 17:18:02 +0200

    VFX Mario Implementado

--- JuegoODS/Assets/UnityTechnologies/Let's Try Shooter/Scripts/CompletedScripts/ShootableBox.cs
+++ JuegoODS/Assets/UnityTechnologies/Let's Try Shooter/Scripts/CompletedScripts/ShootableBox.cs
@@ -5,8 +5,10 @@ public class ShootableBox : MonoBehaviour {
 
 	//The box's current health point total
 	public int currentHealth = 3;
+    // Reference to the particle system
+    public ParticleSystem destructionParticles;
 
-	public void Damage(int damageAmount)
+    public void Damage(int damageAmount)
 	{
 		//subtract damage amount when Damage function is called
 		currentHealth -= damageAmount;
@@ -14,8 +16,18 @@ public class ShootableBox : MonoBehaviour {
 		//Check if health has fallen below zero
 		if (currentHealth <= 0) 
 		{
-			//if health has fallen below zero, deactivate it 
-			gameObject.SetActive (false);
+            // Activate the particle system
+            if (destructionParticles != null)
+            {
+                // Instantiate the particle system at the object's position and rotation
+                ParticleSystem instantiatedParticles = Instantiate(destructionParticles, transform.position, transform.rotation);
+                instantiatedParticles.Play();
+
+                // Optionally, destroy the particle system after it has played
+                Destroy(instantiatedParticles.gameObject, instantiatedParticles.main.duration);
+            }
+            //if health has fallen below zero, deactivate it 
+            gameObject.SetActive (false);
 		}
 	}
 }
