90252c77b26126a54c456bee2d21b490d666b7af
monikkxd <monicagarciaask@gmail.com>
2024-06-26 22:56:10 +0200

    SCRIPT BARRA PROGRESO ANDREA

---
+++ JuegoODS/Assets/_MinijuegoAndrea/ProgressBarController.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ProgressBarController : MonoBehaviour
+{
+    // La barra de progreso (Slider)
+    public Slider progressBar;
+
+    // Lista de objetos a eliminar
+    public List<GameObject> objectsToDestroy;
+
+    // Cantidad de objetos eliminados
+    private int objectsDestroyedCount = 0;
+
+    void Start()
+    {
+        // Inicializa la barra de progreso
+        if (progressBar != null)
+        {
+            progressBar.maxValue = objectsToDestroy.Count;
+            progressBar.value = 0;
+        }
+    }
+
+    // Llama a esta función para eliminar un objeto
+    public void DestroyObject(GameObject obj)
+    {
+        if (objectsToDestroy.Contains(obj))
+        {
+            objectsToDestroy.Remove(obj);
+            Destroy(obj);
+            objectsDestroyedCount++;
+            UpdateProgressBar();
+        }
+    }
+
+    // Actualiza la barra de progreso
+    void UpdateProgressBar()
+    {
+        if (progressBar != null)
+        {
+            progressBar.value = objectsDestroyedCount;
+        }
+    }
+}
+
