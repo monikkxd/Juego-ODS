d5ee35d866687c3656c9d166d76b0187e6d7be28
monikkxd <monicagarciaask@gmail.com>
2024-06-30 20:06:11 +0200

    tuto final andrea

--- JuegoODS/Assets/_MinijuegoAndrea/Scripts/Meta.cs
+++ JuegoODS/Assets/_MinijuegoAndrea/Scripts/Meta.cs
@@ -6,16 +6,36 @@ using UnityEngine.SceneManagement;
 
 public class Meta : MonoBehaviour
 {
+
+    // Referencia a la imagen que queremos activar
+    public Image imageToActivate;
+
     void OnTriggerEnter(Collider other)
     {
         // Verificar si el objeto con el que se ha colisionado tiene la etiqueta "meta"
         if (other.CompareTag("meta"))
         {
-            SelectorNivel.andreaCompletado = true;
-            // Mostrar el texto de "Meta"
-            Debug.Log("Meta");
-            SceneManager.LoadScene("SelecciónNivel");
+
+            StartCoroutine(ActivateImageRoutine());
+            
         }
+    }
+
+    private IEnumerator ActivateImageRoutine()
+    {
+        // Activar la imagen
+        imageToActivate.gameObject.SetActive(true);
+
+        // Esperar 5 segundos
+        yield return new WaitForSeconds(5f);
+
+        SelectorNivel.andreaCompletado = true;
+        // Mostrar el texto de "Meta"
+        Debug.Log("Meta");
+        SceneManager.LoadScene("SelecciónNivel");
+
+        // Aquí puedes agregar la acción que deseas realizar después de los 5 segundos
+        Debug.Log("5 segundos han pasado. Puedes realizar otra acción aquí.");
     }
 }
 
